fe692788862f97abe4644cdf621e6358e4d7ee01
Jannatul-902 <ferdous15-3902@diu.edu.bd>
2025-04-22 18:01:25 +0600

    Admin Edit & Delete Catagory

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -33,7 +33,7 @@
                         </div>
                     </form>
                 </div>
-                <a class="tf-button style-1 w208" href="#"><i
+                <a class="tf-button style-1 w208" href="{{ route('admin.category.add') }}"><i
                         class="icon-plus"></i>Add new</a>
             </div>
             <div class="wg-table table-all-user">
@@ -56,25 +56,25 @@
 
 
                             <tr>
-                                <td>{{$category->$id}}</td>
+                                <td>{{$category->id}}</td>
                                 <td class="pname">
                                     <div class="image">
-                                        <img src="{{ asset('uploads/brands')}}/{{ $category->image }}" alt="{{ $category->name }}" class="image">
+                                        <img src="{{ asset('uploads/categories')}}/{{ $category->image }}" alt="{{ $category->name }}" class="image">
                                     </div>
                                     <div class="name">
                                         <a href="#" class="body-title-2">{{$category->name}}</a>
                                     </div>
                                 </td>
-                                <td>{{$brand->slug}}</td>
+                                <td>{{$category->slug}}</td>
                                 <td><a href="#" target="_blank">0</a></td>
                                 <td>
                                     <div class="list-icon-function">
-                                        <a href="#">
+                                        <a href="{{ route('admin.category.edit', ['id'=>$category->id]) }}">
                                             <div class="item edit">
                                                 <i class="icon-edit-3"></i>
                                             </div>
                                         </a>
-                                        <form action="#" method="POST">
+                                        <form action="{{ route('admin.category.delete', ['id'=>$category->id]) }}" method="POST">
                                             @csrf
                                             @method('DELETE')
                                             <div class="item text-danger delete">
